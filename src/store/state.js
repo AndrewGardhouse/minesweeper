587124a8fb7d6c6c512e5d8f22c4ee1162c711b0
Andrew Gardhouse <andrewgardhouse@macbook-air.gateway.2wire.net>
2019-01-08 12:59:56 -0500

    Adds default state

--- src/store/state.js
+++ src/store/state.js
@@ -1,1 +1,25 @@
-export default {};
+export default {
+  gameOptions: {
+    easy: {
+      columns: 9,
+      rows: 9,
+      bombs: 10,
+    },
+    medium: {
+      columns: 16,
+      rows: 16,
+      bombs: 40,
+    },
+    hard: {
+      columns: 32,
+      rows: 32,
+      bombs: 200,
+    },
+    veryHard: {
+      columns: 32,
+      rows: 32,
+      bombs: 400,
+    },
+  },
+  board: [],
+};
