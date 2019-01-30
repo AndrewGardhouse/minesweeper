52664d62a7214b7b727d06a2eac1a105082898ad
Andrew Gardhouse <andrewgardhouse@macbook-air.gateway.2wire.net>
2019-01-30 18:17:32 -0500

    Adds docs folder for gh-pages

---
+++ docs/precache-manifest.3aaae790870670da5232082b9a609111.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "620fa69b8e6819a432e2",
+    "url": "/js/chunk-vendors.19774237.js"
+  },
+  {
+    "revision": "4f45e283a59119634e24",
+    "url": "/js/app.fc4f8bfa.js"
+  },
+  {
+    "revision": "543ca0110dbad3726d4dd11014c4d956",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0ab64adcb6e798a7a4ae866c431e16f",
+    "url": "/img/octocat.png"
+  },
+  {
+    "revision": "efcb4f03bdefbcc14bac0f5f5405a8b3",
+    "url": "/img/chrome.png"
+  },
+  {
+    "revision": "4f45e283a59119634e24",
+    "url": "/css/app.0c2a6908.css"
+  }
+];
